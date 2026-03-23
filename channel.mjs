e2086702341c360914f6604b3c84f0d688de720c
blvckroby <scatoroby@gmail.com>
2026-03-23 16:45:16 +0100

    Create channel.mjs

---
+++ channel.mjs
@@ -0,0 +1,51 @@
+export const channels = [
+    {
+        id: "vavoo_SKY SPORTS 24|group:it",
+        type: "tv",
+        name: "Sky Sport 24",
+        poster: "https://raw.githubusercontent.com/blvckroby/tnb/master/sky24.jpg",
+        genres: ["Sport"],
+    },
+    {
+        id: "vavoo_SKY SPORTS UNO|group:it",
+        type: "tv",
+        name: "Sky Sport Uno",
+        poster: "https://raw.githubusercontent.com/blvckroby/tnb/master/skyuno.jpg",
+        genres: ["Sport"],
+    },
+    {
+        id: "vavoo_SKY SPORTS F1|group:it",
+        type: "tv",
+        name: "Sky Sport F1",
+        poster: "https://raw.githubusercontent.com/blvckroby/tnb/master/skyf1.jpg",
+        genres: ["Sport"],
+    },
+    {
+        id: "vavoo_SKY SPORT UNO|group:it",
+        type: "tv",
+        name: "Sky Sport Uno",
+        poster: "https://raw.githubusercontent.com/blvckroby/provaTV/main/skyuno.jpg",
+        genres: ["Sport"]
+    },
+    {
+        id: "vavoo_SKY SPORT MOTOGP|group:it",
+        type: "tv",
+        name: "Sky Sport MotoGP",
+        poster: "https://raw.githubusercontent.com/blvckroby/tnb/master/skymotogp.jpg",
+        genres: ["Sport"]
+    },
+    {
+        id: "vavoo_DAZN 1|group:it",
+        type: "tv",
+        name: "DAZN 1",
+        poster: "https://raw.githubusercontent.com/blvckroby/tnb/master/dazn1.jpg",
+        genres: ["Sport"]
+    },
+    {
+        id: "vavoo_DAZN 2|group:it",
+        type: "tv",
+        name: "DAZN 2",
+        poster: "https://raw.githubusercontent.com/blvckroby/tnb/master/dazn2.jpg",
+        genres: ["Sport"]
+    }
+];
